12400da8186d370091d57bcd919545630a6c642e
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-25 13:57:17 +0200

    php partie 1 exo 2

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 2</title>
+  </head>
+  <body>
+    <?php
+      $lastName = 'lamotte';
+      $firstName = 'fabien';
+      $age = 18;
+      echo $lastName;
+      echo '<br />';
+      echo $firstName;
+      echo '<br />';
+      echo $age;
+    ?>
+  </body>
+</html>
